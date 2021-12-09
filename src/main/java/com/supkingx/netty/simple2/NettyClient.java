582061772ad7a5b7cdaf4ffd1e864fa85341a8ca
supkingx <supkingx@gmail.com>
2021-12-09 21:35:01 +0800

    Netty 快速入门实例之TCP服务（异步优化）

---
+++ src/main/java/com/supkingx/netty/simple2/NettyClient.java
@@ -0,0 +1,48 @@
+package com.supkingx.netty.simple2;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @description:
+ * @Author: wangchao
+ * @Date: 2021/12/6
+ */
+public class NettyClient {
+    public static void main(String[] args) throws InterruptedException {
+
+        // 客户端需要一个事件循环跑
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+
+            // 创建客户端启动对象
+            // 注意客户端使用的不是 serverBootstrap 而是 BootStrap
+            Bootstrap bootstrap = new Bootstrap();
+
+            // 设置相关参数
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel channel) throws Exception {
+                            // 在管道的最后添加一个处理器(自定义的)
+                            channel.pipeline().addLast(new NettyClientHandler());
+                        }
+                    });
+            System.out.println("客户端。。ok");
+
+            // 启动客户端去连接服务器端
+            // 关于 channelFuture 要分析，涉及到 netty 的异步模型
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 6677).sync();
+            // 给关闭通道进行监听
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            // 优雅的关闭
+            group.shutdownGracefully();
+        }
+    }
+}
--- src/main/java/com/supkingx/netty/simple2/NettyServerHandler.java
+++ src/main/java/com/supkingx/netty/simple2/NettyServerHandler.java
@@ -1,4 +1,4 @@
-package com.supkingx.netty.simple;
+package com.supkingx.netty.simple2;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -7,6 +7,10 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelPipeline;
 import io.netty.util.CharsetUtil;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 说明
@@ -28,26 +32,37 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        try {
-            Thread.sleep(10 * 1000);
-            ctx.writeAndFlush(Unpooled.copiedBuffer("hello,客户端，你好！", CharsetUtil.UTF_8));
-        } catch (InterruptedException e) {
-            System.out.println("发生异常" + e.getMessage());
-        }
-        
-        
-        System.out.println("服务器读取线程 " + Thread.currentThread().getName());
-        System.out.println("server ctx=" + ctx);
-        System.out.println("看看channel 和 pipeline的关系");
-        Channel channel = ctx.channel();
-        ChannelPipeline pipeline = ctx.pipeline();
+        // 比如我们有一个耗时很长的任务 -> 异步执行 -> 提交该channel 对应的 NIOEventloop 的 taskQueue中
+        // 解决方案1 用哪个户程序自定义的普通任务
 
+        ctx.channel().eventLoop().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10 * 1000);
+                    String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                    ctx.writeAndFlush(Unpooled.copiedBuffer(now + " hello,客户端，你好！", CharsetUtil.UTF_8));
+                } catch (InterruptedException e) {
+                    System.out.println("发生异常" + e.getMessage());
+                }
+            }
+        });
 
-        // 将 msg 转化成一个 ByteBuffer
-        // ByteBuf 是 Netty 提供的，不是NIO 的 ByteBuffer
-        ByteBuf buf = (ByteBuf) msg;
-        System.out.println("客户端发送消息是：" + buf.toString(CharsetUtil.UTF_8));
-        System.out.println("客户端地址：" + ctx.channel().remoteAddress());
+        ctx.channel().eventLoop().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(20 * 1000);
+                    String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                    ctx.writeAndFlush(Unpooled.copiedBuffer(now + " hello,客户端，你好！", CharsetUtil.UTF_8));
+                } catch (InterruptedException e) {
+                    System.out.println("发生异常" + e.getMessage());
+                }
+            }
+        });
+
+        String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println("server....go," + now);
     }
 
     /**
